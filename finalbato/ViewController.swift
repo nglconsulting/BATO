0fc2e293e24a85ac24f51f92c2d095c2ec774bb3
Ludus Académie <Ludus@mac-mini-de-ludus.local>
2018-05-09 00:22:13 +0200

    Initial Commit

---
+++ finalbato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalbato
+//
+//  Created by Ludus Académie on 09/05/2018.
+//  Copyright © 2018 Ludus Académie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
